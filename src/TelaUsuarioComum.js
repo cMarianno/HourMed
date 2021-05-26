85d5153c9cfefa12593a44f4fc1e00439b821a39
lauraxavierr <laurausual@gmail.com>
2021-05-25 22:51:36 -0300

    feat: centralizando dela de usuário comum.

--- src/TelaUsuarioComum.js
+++ src/TelaUsuarioComum.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import DropDownPicker from 'react-native-dropdown-picker';
-import { StyleSheet, TouchableOpacity, StatusBar, Text, View, TextInput} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 import { useFonts } from 'expo-font';
 import { useNavigation } from '@react-navigation/native'
 
@@ -25,24 +26,19 @@ export default function TelaUsuarioComum() {
         who: 'usuario'
     }
 
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity
-                style={styles.icon}
-                activeOpacity={0.8}
-            >
-                <Icon name="arrow-left" size={20} color="#12003C" />
-            </TouchableOpacity>
-
-            <Text style={styles.title}>Crie sua conta</Text>
-        
-            <DropDownPicker
+  return (
+    <View style={styles.container}>
+        <View style={styles.header}>
+            <Text style={styles.headerTextAdd}>Voltar</Text>
+            <Text style={styles.headerTextResp}>Crie sua Conta</Text>
+      </View>
+      <DropDownPicker
                 items={[
                     {label: 'Usuário', value: 'usuario', icon: () => <Icon name="flag" size={18} color="#D5E8FE" />, hidden: true},
                     {label: 'Responsável', value: 'responsavel', icon: () => <Icon name="flag" size={18} color="#D5E8FE" />}
                 ]}
                 defaultValue={TelaUsuarioComum.state.who}
-                containerStyle={{height: 40, marginTop: '5%'}}
+                containerStyle={{height: 40, marginTop: '5%', width: '80%'}}
                 style={{ backgroundColor: '#D5E8FE'}}
                 itemStyle={{
                     justifyContent: 'flex-start'
@@ -51,163 +47,113 @@ export default function TelaUsuarioComum() {
                 onChangeItem={item => TelaUsuarioComum.setState({
                     who: item.value
             })}>
+        
+        </DropDownPicker>
+       <View style={styles.espacos2}></View>
 
-            </DropDownPicker>
+       <Text style={styles.txtSubtitulo}>Nome:</Text>
+       <TextInput style={styles.txtInput}/>
+       <View style={styles.espacos2}></View>
 
-            <Text style={styles.txtName}>Nome:</Text>
-            <TextInput style={styles.inputName} />
+       <Text style={styles.txtSubtitulo}>Email:</Text>
+       <TextInput style={styles.txtInput}/>
+       <View style={styles.espacos2}></View>
+        
+       <Text style={styles.txtSubtitulo}>Senha:</Text>
+       <TextInput style={styles.txtInput}/>
+       <View style={styles.espacos2}></View>
 
-            <Text style={styles.txtEmail}>Email:</Text>
-            <TextInput style={styles.inputEmail} styles={styles.inputEmail}/>
-
-            <Text style={styles.txtSenha}>Senha:</Text>
-            <TextInput style={styles.inputSenha} styles={styles.inputSenha}/>
-
-            <Text style={styles.txtSenhaConf}>Confirmar senha:</Text>
-            <TextInput style={styles.inputSenhaConf} styles={styles.inputSenhaConf}/>
-
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.8}
-                onPress={handlePressConcluir}
-            >
-                <Text style={styles.textConcluir}>CONCLUIR</Text>
-            </TouchableOpacity>
-
-            <StatusBar style="auto" />
-        </View>
-    );
+       <Text style={styles.TxtConfirmarSenha}>Confirmar Senha:</Text>
+       <TextInput style={styles.txtInput}/>
+       <View style={styles.espacos2}></View>
+       <TouchableOpacity style={styles.BtnConcluir} activeOpacity={0.8} onPress={handlePressConcluir}>
+           <Text style={styles.BtnTxt}>Concluir</Text>
+       </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        backgroundColor: '#EDFBFD',
-    },
-    icon:{
-        marginTop: '10%',
-        marginRight: '85%',
-        alignItems: 'center',
-    },
-    iconRight: {
-        marginTop: '-18%',
-        position: 'relative',
-        marginLeft: '280%',
-        alignItems: 'center',
-    },
-    title: {
-        fontFamily: 'Roboto',
-        marginTop: '-6%',
-        fontWeight: '400',
-        fontSize: 20,
-        height: 20,
-        textAlign: 'center',
-        color: '#00498c'
-    },
-    txtName:{
-        marginTop: '10%',
-        width: 350,
-        height: 30,
-        marginLeft: '10%',
-        fontFamily: 'Roboto',
-        fontSize: 14,
-        lineHeight: 17,
-        color: '#00498c'
-    },
-    inputName:{
-        // marginTop: '1%',
-        height: 40,
-        borderWidth: 1,
-        borderColor: '#00498c',
-        width: 280,
-        marginLeft: '7%',
-        borderRadius: 20,
-        backgroundColor: '#D5E8FE',
-        padding: 10
-    },
-    txtEmail:{
-        marginTop: '5%',
-        width: 350,
-        height: 30,
-        marginLeft: '10%',
-        fontFamily: 'Roboto',
-        fontSize: 14,
-        lineHeight: 17,
-        color: '#00498c'
-    },
-    inputEmail:{
-        // marginTop: '45%',
-        height: 40,
-        borderWidth: 1,
-        borderColor: '#00498c',
-        width: 280,
-        marginLeft: '7%',
-        borderRadius: 20,
-        backgroundColor: '#D5E8FE',
-        padding: 10
-    },
-    txtSenha:{
-        marginTop: '5%',
-        width: 350,
-        height: 30,
-        marginLeft: '10%',
-        fontFamily: 'Roboto',
-        fontSize: 14,
-        lineHeight: 17,
-        color: '#00498c'
-    },
-    inputSenha:{
-        // marginTop: '70%',
-        height: 40,
-        borderWidth: 1,
-        borderColor: '#00498c',
-        width: 280,
-        marginLeft: '7%',
-        borderRadius: 20,
-        backgroundColor: '#D5E8FE',
-        padding: 10
-    },
-    txtSenhaConf:{
-        marginTop: '5%',
-        width: 350,
-        height: 30,
-        marginLeft: '10%',
-        fontFamily: 'Roboto',
-        fontSize: 14,
-        lineHeight: 17,
-        color: '#00498c'
-    },
-    inputSenhaConf:{
-        // marginTop: '100%',
-        height: 40,
-        borderWidth: 1,
-        borderColor: '#00498c',
-        width: 280,
-        marginLeft: '7%',
-        borderRadius: 20,
-        backgroundColor: '#D5E8FE',
-        padding: 10
-    },
-    button: {
-        borderRadius: 20,
-        marginTop: '12%',
-        backgroundColor: "#12003C",
-        width: '50%',
-        marginLeft: '25%'
-    },
-    textConcluir: {
-        color: "#fff",
-        fontSize: 18,
-        fontWeight: 'bold',
-        width: '30%',
-        paddingLeft: 47,
-        paddingTop: 10,
-        paddingRight: 100,
-        paddingBottom: 10,
-        textAlign: 'center'
-    }
-
-
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#EDFBFD',
+  },
+  header: {
+    flex: 0.15,
+    alignItems: 'center',
+    backgroundColor: '#EDFBFD',
+    paddingTop:50,
+    width: "100%", 
+  },
+  headerTextAdd: {
+    fontFamily: 'Roboto',
+    fontSize: 18,
+    paddingTop: 2,
+    paddingRight:300,  
+    color: '#00498c',
+    fontWeight: 'bold',
+  },
+  txtSubtitulo:{
+    fontFamily: 'Roboto',
+    fontSize: 17,
+    paddingTop: 2,
+    paddingRight:260,  
+    color: '#00498c',
+    fontWeight: 'bold',
+    flex: 0.06
+  },
+  txtInput:{
+    fontFamily: 'Roboto',
+    fontSize: 1,
+    paddingRight:310,  
+    color: '#00498c',
+    fontWeight: 'bold',
+    flex: 0.05,
+    backgroundColor: "#055468",
+    borderRadius: 20,
+    backgroundColor: '#D5E8FE'
+  },
+  TxtConfirmarSenha:{
+    fontFamily: 'Roboto',
+    fontSize: 17,
+    paddingTop: 2,
+    paddingRight:160,  
+    color: '#00498c',
+    fontWeight: 'bold',
+    flex: 0.06
+  },
+  BtnConcluir:{
+    backgroundColor: "#000000",
+    paddingRight:150,  
+    color: '#ffffff',
+    fontWeight: 'bold',
+    flex: 0.05,
+    borderRadius: 20,
+    textAlign: 'center',
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: 15,
+    fontFamily: 'Roboto'
+  },
+  BtnTxt: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    fontSize: 15,
+    fontFamily: 'Roboto'
+  },
+  headerTextResp: {
+    fontFamily: 'Roboto',
+    fontSize: 20,
+    color: '#00498c',
+    paddingTop:10,
+    fontWeight: 'bold',
+  },
+  espacos2:{
+    flex:0.03,
+    width: "100%"
+  }
 
 });
